198c5ddecb3e297011777443e14895cc16b59d91
sworc <lightbox@nifty.com>
2021-08-31 16:02:52 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace cs_form_framework_net_json_datagridview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 81);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(817, 413);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 538);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace cs_form_framework_net_json_datagridview
+{
+    public partial class Form1 : Form
+    {
+        private class Syain
+        {
+            public String 社員コード { get; set; }
+            public String 氏名 { get; set; }
+            public String フリガナ { get; set; }
+            public String 所属 { get; set; }
+            private string _性別;
+            public string 性別
+            {
+                get
+                {
+                    if (this._性別 == "0")
+                    {
+                        return "男";
+                    }
+                    else
+                    {
+                        return "女";
+                    }
+                }
+                set
+                {
+                    this._性別 = value;
+                }
+            }
+            public int 給与 { get; set; }
+            public int? 手当 { get; set; }
+            public String 管理者 { get; set; }
+
+            public String 作成日 { get; set; }
+            public String 更新日 { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.Columns.Clear();
+            dataGridView1.ColumnCount = 10;
+            dataGridView1.Columns[0].Name = "社員コード";
+            dataGridView1.Columns[1].Name = "氏名";
+            dataGridView1.Columns[2].Name = "フリガナ";
+            dataGridView1.Columns[3].Name = "所属";
+            dataGridView1.Columns[4].Name = "性別";
+            dataGridView1.Columns[5].Name = "給与";
+            dataGridView1.Columns[6].Name = "手当";
+            dataGridView1.Columns[7].Name = "管理者";
+            dataGridView1.Columns[8].Name = "作成日";
+            dataGridView1.Columns[9].Name = "更新日";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // インターネットアクセス用クラス( WebClient )
+            WebClient webClient = new WebClient();
+            webClient.Encoding = Encoding.GetEncoding("utf-8");
+
+            Uri uri = new Uri("https://lightbox.sakura.ne.jp/demo/json/syain_json.php");
+
+            string json = webClient.DownloadString(uri);
+
+            Syain[] data = JsonConvert.DeserializeObject<Syain[]>(json);
+
+            // 行をクリア
+            dataGridView1.Rows.Clear();
+
+            // 一行ぶんのデータの配列用
+            ArrayList al = new ArrayList();
+
+            // 一覧
+            foreach ( Syain row in data )
+            {
+                // １行のクリア
+                al.Clear();
+                al.Add(row.社員コード);
+                al.Add(row.氏名);
+                al.Add(row.フリガナ);
+                al.Add(row.所属);
+                al.Add(row.性別);
+                al.Add(row.給与);
+                al.Add(row.手当);
+                al.Add(row.管理者);
+                al.Add(row.作成日);
+                al.Add(row.更新日);
+                // １行をセット
+                dataGridView1.Rows.Add(al.ToArray());
+            }
+
+            // 値の長さでカラム幅の自動調整
+            dataGridView1.AutoResizeColumns();
+        }
+    }
+}
